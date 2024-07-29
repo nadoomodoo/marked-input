0da826e00c33bd9eb22af910dea94ddec99b5e1d
HamelMoon <hamelmoon@nadoomodoo.com>
2024-07-29 17:42:42 +0900

    [Fix] pieces type error

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -18,7 +18,7 @@ export const assign = Object.assign
 export function denote(value: string, callback: (mark: MarkMatch) => string, ...markups: Markup[]): string {
 	if (!markups.length) return value
 	const pieces = new Parser(markups).split(value)
-	return pieces.reduce((previous: string, current) => previous += isObject(current) ? callback(current) : current, '')
+	return pieces.reduce((previous: string, current) => previous += isObject(current) ? callback(current) : current, '')?.toString()
 }
 
 export function toString(values: MarkStruct[], options: Option[]) {
